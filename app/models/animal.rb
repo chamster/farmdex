5033f84d146c25cf4553c46c3ef2a337b1e3b789
Robert Rogers <brseaside@gmail.com>
2014-04-27 18:54:40 -0700

    get ready for next commit

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -5,8 +5,6 @@ class Animal < ActiveRecord::Base
 	belongs_to :origin, :class_name => Origin, :foreign_key => :origin_id
 	belongs_to :source, :class_name => Source, :foreign_key => :source_id
 	belongs_to :for_sale_status, :class_name => ForSaleStatus, :foreign_key => :for_sale_status_id
-
-	validates_presence_of :eartag
-
+    validates_presence_of :eartag
 
 end
